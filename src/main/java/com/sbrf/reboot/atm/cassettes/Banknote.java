fc1a22f6d0caf65318d0e42334f9fddf96ff2c25
AleksMikhailenko <maa1404@mail.ru>
2021-08-26 17:48:55 +0300

    JDR-5: changed class Banknote to abstract

--- src/main/java/com/sbrf/reboot/atm/cassettes/Banknote.java
+++ src/main/java/com/sbrf/reboot/atm/cassettes/Banknote.java
@@ -1,11 +1,4 @@
 package com.sbrf.reboot.atm.cassettes;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class Banknote {
-
-    private String type;
+public abstract class Banknote {
 }
--- src/main/java/com/sbrf/reboot/atm/cassettes/Cassette.java
+++ src/main/java/com/sbrf/reboot/atm/cassettes/Cassette.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import java.util.List;
 
 @AllArgsConstructor
-public class Cassette<E> {
+public class Cassette<E extends Banknote> {
     private List<E> banknotes;
 
     public int getCountBanknotes() {
