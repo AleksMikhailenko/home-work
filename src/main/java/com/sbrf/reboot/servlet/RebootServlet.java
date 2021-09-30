46d28932fc9181c60676a48d3db903e5218ddba5
AleksMikhailenko <maa1404@mail.ru>
2021-09-30 15:19:17 +0300

    JDR-15: fixed a raw parameter name

--- src/main/java/com/sbrf/reboot/servlet/RebootServlet.java
+++ src/main/java/com/sbrf/reboot/servlet/RebootServlet.java
@@ -8,22 +8,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.owasp.encoder.Encode.forHtml;
+
 @WebServlet("/reboot")
 public class RebootServlet extends HttpServlet {
 
     private static final AtomicInteger counter = new AtomicInteger();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         counter.getAndIncrement();
-        String name = request.getParameter("name");
+        String rawName = request.getParameter("name");
         response.setContentType("text/html");
-        try (PrintWriter printWriter = response.getWriter()) {
-            printWriter.write(String.format("Hello %s<br>", name == null ? "" : name));
-            printWriter.write(String.format("Counter = %s", counter));
-        } catch (IOException e) {
-            // some logic for error
-            e.printStackTrace();
-        }
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(String.format("Hello %s<br>", rawName == null ? "" : forHtml(rawName)));
+        printWriter.write(String.format("Counter = %s", counter));
     }
 }
