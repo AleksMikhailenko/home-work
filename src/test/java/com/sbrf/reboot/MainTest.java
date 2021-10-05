30ea8e121624a9bdfc0dedb2b52cedf6a74139b6
AleksMikhailenko <maa1404@mail.ru>
2021-10-05 23:15:12 +0300

    JDR-16: added mock-test

--- src/test/java/com/sbrf/reboot/MainTest.java
+++ src/test/java/com/sbrf/reboot/MainTest.java
@@ -2,7 +2,7 @@ package com.sbrf.reboot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
@@ -14,5 +14,16 @@ class MainTest {
     @Test
     void getContextFromJavaBasedConfig() {
         assertNotNull(Main.getContextFromJavaBasedConfig());
+    }
+
+    @Test
+    void shouldBeNotEquals() {
+        assertNotSame(Main.getContextFromXML(), Main.getContextFromJavaBasedConfig());
+    }
+
+    @Test
+    void main() {
+        Main.main(new String[]{"1", "2"});
+        assertTrue(true);
     }
 }
